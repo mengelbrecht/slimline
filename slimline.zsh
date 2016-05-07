e9eaea2541c50031d1ad21d120a5c4d9f2c2f972
Markus Engelbrecht <github@enbre.com>
2016-05-07 09:18:07 +0200

    Use if expressions for better readability

--- slimline.zsh
+++ slimline.zsh
@@ -32,8 +32,9 @@ prompt_slimline_check_cmd_exec_time() {
   local integer elapsed
   (( elapsed = EPOCHSECONDS - ${_prompt_slimline_cmd_timestamp:-$EPOCHSECONDS} ))
   _prompt_slimline_cmd_exec_time=
-  (( elapsed > ${SLIMLINE_MAX_EXEC_TIME:-5} )) && \
+  if (( elapsed > ${SLIMLINE_MAX_EXEC_TIME:-5} )); then
     _prompt_slimline_cmd_exec_time="$(prompt_slimline_human_time $elapsed)"
+  fi
 }
 
 prompt_slimline_set_prompt() {
@@ -43,8 +44,9 @@ prompt_slimline_set_prompt() {
   PROMPT=""
 
   # add ssh info
-  (( ${SLIMLINE_DISPLAY_SSH_INFO:-1} )) && [[ -n "$SSH_TTY" ]] && \
+  if (( ${SLIMLINE_DISPLAY_SSH_INFO:-1} )) && [[ -n "$SSH_TTY" ]]; then
     PROMPT+="%F{red}%n%f@%F{yellow}%m%f "
+  fi
 
   # add cwd
   PROMPT+="%F{cyan}%3~%f "
@@ -58,16 +60,19 @@ prompt_slimline_set_rprompt() {
   RPROMPT=""
 
   # add elapsed time if threshold is exceeded
-  (( ${SLIMLINE_DISPLAY_EXEC_TIME:-1} )) && [[ -n "${_prompt_slimline_cmd_exec_time}" ]] && \
+  if (( ${SLIMLINE_DISPLAY_EXEC_TIME:-1} )) && [[ -n "${_prompt_slimline_cmd_exec_time}" ]]; then
     RPROMPT+="%F{yellow}${_prompt_slimline_cmd_exec_time}%f"
+  fi
 
   # add exit status
-  (( ${SLIMLINE_DISPLAY_EXIT_STATUS:-1} )) && \
+  if (( ${SLIMLINE_DISPLAY_EXIT_STATUS:-1} )); then
     RPROMPT+="%(?::${RPROMPT:+ }%F{red}%? ↵%f)"
+  fi
 
   # add git radar output
-  [[ -n "${_prompt_slimline_git_radar_output:-}" ]] && \
+  if [[ -n "${_prompt_slimline_git_radar_output:-}" ]]; then
     RPROMPT+="${RPROMPT:+ }${_prompt_slimline_git_radar_output}"
+  fi
 }
 
 prompt_slimline_set_sprompt() {
@@ -92,11 +97,11 @@ prompt_slimline_preexec() {
 
 prompt_slimline_async_git_radar() {
   local _prompt_slimline_git_radar_output=""
-  (( ${SLIMLINE_ENABLE_GIT_RADAR:-1} )) && {
+  if (( ${SLIMLINE_ENABLE_GIT_RADAR:-1} )); then
     local parameters="--zsh"
     (( ${SLIMLINE_PERFORM_GIT_FETCH:-1} )) && parameters+=" --fetch"
     _prompt_slimline_git_radar_output="$(${_prompt_slimline_git_radar_executable} ${=parameters})"
-  }
+  fi
   typeset -p _prompt_slimline_git_radar_output >! "$_prompt_slimline_async_data"
 
   kill -WINCH $$ # Signal completion to parent process.
@@ -104,7 +109,9 @@ prompt_slimline_async_git_radar() {
 
 prompt_slimline_async_tasks() {
   # Kill the old process of slow commands if it is still running.
-  (( __prompt_slimline_async_pid > 0 )) && kill -KILL "$_prompt_slimline_async_pid" &>/dev/null
+  if (( __prompt_slimline_async_pid > 0 )); then
+    kill -KILL "$_prompt_slimline_async_pid" &>/dev/null
+  fi
 
   trap prompt_slimline_async_callback WINCH
   prompt_slimline_async_git_radar &!
@@ -112,17 +119,19 @@ prompt_slimline_async_tasks() {
 }
 
 prompt_slimline_async_callback() {
-  (( _prompt_slimline_async_pid > 0 )) && {
-    [[ -s "$_prompt_slimline_async_data" ]] && {
-      alias typeset='typeset -g'
-      source "$_prompt_slimline_async_data"
-      unalias typeset
-    }
-    _prompt_slimline_async_pid=0
-    prompt_slimline_set_prompt white
-    prompt_slimline_set_rprompt
-    zle && zle .reset-prompt
-  }
+  if (( _prompt_slimline_async_pid == 0 )); then
+    return
+  fi
+
+  if [[ -s "$_prompt_slimline_async_data" ]]; then
+    alias typeset='typeset -g'
+    source "$_prompt_slimline_async_data"
+    unalias typeset
+  fi
+  _prompt_slimline_async_pid=0
+  prompt_slimline_set_prompt white
+  prompt_slimline_set_rprompt
+  zle && zle .reset-prompt
 }
 
 prompt_slimeline_async_init() {
@@ -143,8 +152,9 @@ prompt_slimline_setup() {
 
   _prompt_slimline_git_radar_executable="$prompt_slimline_path/git-radar/git-radar"
 
-  [[ -z "$GIT_RADAR_FORMAT" ]] && \
+  if [[ -z "$GIT_RADAR_FORMAT" ]]; then
     export GIT_RADAR_FORMAT="%{remote: }%{branch}%{ :local}%{ :changes}%{ :stash}"
+  fi
 
   prompt_slimeline_async_init
 
