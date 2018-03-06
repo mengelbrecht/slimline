ba83f68bf2a0c1693140abfd0de0f41f3b204ebe
Markus Engelbrecht <mgee@users.noreply.github.com>
2018-03-06 23:05:36 +0100

    Use expand function for prompts

--- lib/prompt.zsh
+++ lib/prompt.zsh
@@ -2,10 +2,9 @@ slimline::prompt::set_left() {
   local sections="${1}"
   local separator="${SLIMLINE_LEFT_PROMPT_SECTION_SEPARATOR:- }"
   shift 1
-  slimline::sections::get_output "${sections}" "${separator}" "slimline_left_prompt_sections_output" "$@"
 
-  local format="|sections| "
-  PROMPT="${${SLIMLINE_LEFT_PROMPT_FORMAT:-${format}}/|sections|/${slimline_left_prompt_sections_output}}"
+  slimline::sections::get_output "${sections}" "${separator}" "slimline_left_prompt_sections_output" "$@"
+  PROMPT="$(slimline::utils::expand "left_prompt" "|sections| " "sections" "${slimline_left_prompt_sections_output}")"
   unset slimline_left_prompt_sections_output
 }
 
@@ -13,19 +12,16 @@ slimline::prompt::set_right() {
   local sections="${1}"
   local separator="${SLIMLINE_RIGHT_PROMPT_SECTION_SEPARATOR:- }"
   shift 1
-  slimline::sections::get_output "${sections}" "${separator}" "slimline_right_prompt_sections_output" "$@"
 
-  local format="|sections|"
-  RPROMPT="${${SLIMLINE_RIGHT_PROMPT_FORMAT:-${format}}/|sections|/${slimline_right_prompt_sections_output}}"
+  slimline::sections::get_output "${sections}" "${separator}" "slimline_right_prompt_sections_output" "$@"
+  RPROMPT="$(slimline::utils::expand "right_prompt" "|sections|" "sections" "${slimline_right_prompt_sections_output}")"
   unset slimline_right_prompt_sections_output
 }
 
 slimline::prompt::set_spelling() {
-  local from="%R"
-  local to="%r"
-  local format="zsh: correct %F{red}|from|%f to %F{green}|to|%f [nyae]? "
-  local selected="${SLIMLINE_AUTOCORRECT_FORMAT:-${format}}"
-  SPROMPT="${${selected/|from|/${from}}/|to|/${to}}"
+  SPROMPT="$(slimline::utils::expand "autocorrect" \
+      "zsh: correct %F{red}|from|%f to %F{green}|to|%f [nyae]? " \
+      "from" "%R" "to" "%r")"
 }
 
 slimline::prompt::set() {
